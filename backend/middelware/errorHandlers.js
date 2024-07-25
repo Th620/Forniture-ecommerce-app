09df217d3358aec40f01589bdf8383690ee20810
th <t.hadj11010@gmail.com>
2024-07-25 19:56:12 +0100

    error handeling middelwares

---
+++ backend/middelware/errorHandlers.js
@@ -0,0 +1,15 @@
+const errorResposerHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 400;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+const invalidPathHandler = (req, res, next) => {
+  let error = new Error("Invalid Path");
+  error.statusCode = 404;
+  next(error);
+};
+
+module.exports = { errorResposerHandler, invalidPathHandler };
