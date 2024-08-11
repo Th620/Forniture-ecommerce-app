8432d4360df38a0a264fe606d6a868196f8b46ed
th <t.hadj11010@gmail.com>
2024-08-11 17:30:30 +0100

    not found page

---
+++ frontend/app/not-found.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='bg-white min-h-screen w-full flex justify-center items-center'>
+        <div className='font-montserrat font-bold text-3xl text-blueBg'>404 - Page not found</div>
+    </div>
+  )
+}
